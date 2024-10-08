967d3a796be7bb5f98192f139fe9b187bce3ea4a
Charles Zambrano <charleszambranoespe@outlook.com>
2024-10-07 21:07:28 -0500

    [ADD] ErrorResponse and GlobalExceptionHandler

---
+++ clientes_personas/src/main/java/com/tcs/microservices/clientes_personas/util/exception/ErrorResponse.java
@@ -0,0 +1,30 @@
+package com.tcs.microservices.clientes_personas.util.exception;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Getter
+@Builder
+public class ErrorResponse {
+
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String code;
+    private String message;
+    private List<String> details;
+    private String path;
+    private String method;
+    private ExceptionDetail exception;
+    private String userMessage;
+
+    @Getter
+    @Builder
+    public static class ExceptionDetail {
+        private String type;
+        private StackTraceElement[] trace;
+    }
+}
